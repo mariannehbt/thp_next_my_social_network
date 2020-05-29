f96779cbf688ffce28cfd904645caa6ccde890dd
mariannehbt <marianne.heribert@gmail.com>
2020-05-29 07:40:48 +0200

    add postLists in profiles

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as API from '../services/api';
 import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
 
 const Profile = () => {
 	const profile = useSelector(state => state.profile.profile);
+	const posts = useSelector(state => state.posts.posts);
 
 	const dispatch = useDispatch();
 
@@ -15,6 +17,26 @@ const Profile = () => {
 		const description = document.getElementById('description').value;
 		dispatch(API.profileUpdate(user, username, description));
 	};
+
+	const myPostsList = posts.filter(elem => elem.user != null && elem.user.id === profile.id).sort((a,b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()).map((post, key) => (
+		<div key={key}>
+			<div className='card my-3'>
+				<div className='card-body'>
+					<h5 className='card-title' onClick={() => dispatch(API.fetchUsers(post.user.id))}>{ (post.user) ? <Link to={`/user/${post.user.id}`}>{post.user.username}</Link> : null }</h5>
+					<p className='card-text'>{post.text}</p>
+					<button className='btn btn-info mr-2'>
+						Like
+						<span className='badge badge-light badge-pill mr-2'>{post.like}</span>
+					</button>
+					<button className='btn btn-outline-warning mr-2'>Info</button>
+					<button className='btn btn-outline-danger mr-2' onClick={() => dispatch(API.postDelete(post.id))}>Delete</button>
+				</div>
+				<div className='card-footer'>
+					<p className='card-text'><small className='text-muted'>Last updated {moment(post.updated_at).fromNow()}</small></p>
+				</div>
+			</div>
+		</div>
+	));
 
 	return (
 		<div>
@@ -50,8 +72,10 @@ const Profile = () => {
 						<p className='form-control'>{moment(profile.created_at).calendar()}</p>
 					</div>
 				</div>
-				<button className='btn btn-info' type='submit'>Confirm changes</button>
+				<button className='btn btn-info mr-3' type='submit'>Confirm changes</button>
 			</form>
+			<button className='btn btn-outline-info mt-3' onClick={() => dispatch(API.fetchPosts())}>See posts</button>
+			{myPostsList}
 		</div>
 	);
 };
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as API from '../services/api';
 import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
 
 const User = () => {
 	const user = useSelector(state => state.users.users);
+	const posts = useSelector(state => state.posts.posts);
+
+	const dispatch = useDispatch();
+
+	const userPostsList = posts.filter(elem => elem.user != null && elem.user.id === user.id).sort((a,b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()).map((post, key) => (
+		<div key={key}>
+			<div className='card my-3'>
+				<div className='card-body'>
+					<h5 className='card-title' onClick={() => dispatch(API.fetchUsers(post.user.id))}>{ (post.user) ? <Link to={`/user/${post.user.id}`}>{post.user.username}</Link> : null }</h5>
+					<p className='card-text'>{post.text}</p>
+					<button className='btn btn-info mr-2'>
+						Like
+						<span className='badge badge-light badge-pill mr-2'>{post.like}</span>
+					</button>
+					<button className='btn btn-outline-warning mr-2'>Info</button>
+					<button className='btn btn-outline-danger mr-2' onClick={() => dispatch(API.postDelete(post.id))}>Delete</button>
+				</div>
+				<div className='card-footer'>
+					<p className='card-text'><small className='text-muted'>Last updated {moment(post.updated_at).fromNow()}</small></p>
+				</div>
+			</div>
+		</div>
+	));
 
 	return (
 		<div>
@@ -39,6 +63,8 @@ const User = () => {
 					<p className='form-control'>{moment(user.created_at).calendar()}</p>
 				</div>
 			</div>
+			<button className='btn btn-outline-info' onClick={() => dispatch(API.fetchPosts())}>See posts</button>
+			{userPostsList}
 		</div>
 	);
 };

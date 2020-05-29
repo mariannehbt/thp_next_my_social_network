ce93a3111fd8960d6d406beaacefbaa73884acfc
mariannehbt <marianne.heribert@gmail.com>
2020-05-29 07:59:43 +0200

    Updates

---
+++ precache-manifest.fa1b366214d332fb68676a460b892f10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179aab70249773ca5eeff6e06de61402",
+    "url": "/thp_next_my_social_network/index.html"
+  },
+  {
+    "revision": "e35c2e8b9ec37a89c601",
+    "url": "/thp_next_my_social_network/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1bd04263215978a1463f",
+    "url": "/thp_next_my_social_network/static/css/main.fbfee56e.chunk.css"
+  },
+  {
+    "revision": "e35c2e8b9ec37a89c601",
+    "url": "/thp_next_my_social_network/static/js/2.77dba962.chunk.js"
+  },
+  {
+    "revision": "6d52fb537b3e756cdfddce0a200b707a",
+    "url": "/thp_next_my_social_network/static/js/2.77dba962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bd04263215978a1463f",
+    "url": "/thp_next_my_social_network/static/js/main.52ff3184.chunk.js"
+  },
+  {
+    "revision": "716dfda6ac5c9a01027e",
+    "url": "/thp_next_my_social_network/static/js/runtime-main.2f251d23.js"
+  }
+]);
